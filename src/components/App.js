fab2d36ca987e6026a643879462b7d8fa1dc7841
Jordan Buckmaster <buckmast@purdue.edu>
2018-09-17 20:02:19 -0400

    Add Killian's code

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import '../css/App.css';
+import '../css/common.css';
+import Navbar from './Navbar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+        <Navbar />
       </div>
     );
   }
---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import '../css/common.css'
+import logout from '../img/logout.svg'
+import settings from '../img/settings.svg'
+
+class Navbar extends Component {
+  render() {
+    return (
+    <div className="Navbar">
+        <div className="NavList">
+            <div className="Options">
+                <div className="Settings">
+                    <img src={settings} />
+                </div>
+                <div className="Logout">
+                    <img src={logout} />
+                </div>
+            </div>
+        </div>
+        <div className="Logger-Title">
+            Logger
+        </div>
+    </div>
+    )
+  }
+}
+
+export default Navbar
