d32422d56db8dd56c104ec28f072d5c1f876aa86
Killian Le Clainche <kleclain@purdue.edu>
2018-09-20 19:12:41 -0400

    Preference Panel

--- src/components/PreferencePanel.js
+++ src/components/PreferencePanel.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react'
 import '../css/common.css'
+import exitIcon from "../img/x.svg"
 
 class PreferencePanel extends Component {
     render() {
-        return (<div className="Panel">
-                <div className="Panel-Container">
-                    <div className="Preference-Panel">
-                        <div onClick={ () => this.props.history.push("/filters")}>Set Logger Filters</div>
-                        <div className="Delete-Account" onClick={ () => this.props.history.push("/deleteaccount")}>Delete Account</div>
+        return (<div className="panel">
+                <div className="panel-container">
+                    <div className="panel-preference">
+                        <div className="panel-left-nav" />
+                        <div className="panel-center-content">
+	                        <div onClick={ () => this.props.history.push("/filters")}>Set Logger Filters</div>
+	                        <div className="delete-account" onClick={ () => this.props.history.push("/deleteaccount")}>Delete Account</div>
+                        </div>
+                        <div className="panel-right-nav">
+	                        <div className="exit-nav" onClick={ () => this.props.history.push("/data") }>
+		                        <img src={exitIcon} />
+	                        </div>
+                        </div>
                     </div>
                 </div>
             </div>
