2e3eab0c35b55c1a9926f50ba5e054b3a2454c4b
Jordan Buckmaster <buckmast@purdue.edu>
2018-09-20 23:01:40 -0400

    Add graphs

--- src/components/DataPanel.js
+++ src/components/DataPanel.js
@@ -1,19 +1,66 @@
 import React, { Component } from 'react'
-import { Doughnut } from 'react-chartjs-2';
+import { Doughnut, HorizontalBar, defaults } from 'react-chartjs-2';
 import '../css/common.css'
 
 class DataPanel extends Component {
 
-    getData = () => {
+    getBarData = () => {
         return {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [{
+                    label: 'Social Media',
+                    backgroundColor:  'rgba(255, 159, 64, 0.2)',
+                    data: [1, 1, 1, 1, 1, 1]
+                },
+                {
+                    label: 'Streaming',
+                    backgroundColor: 'rgba(153, 102, 255, 0.2)',
+                    data:  [1, 1, 1, 1, 1, 1]  
+                },
+                {
+                    label: 'Gaming',
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    data:  [1, 1, 1, 1, 1, 1]
+                    },
+                {
+                label: 'News',
+                backgroundColor: 'rgba(255, 206, 86, 0.2)',
+                data:  [1, 1, 1, 1, 1, 1]  
+                },
+                {
+                label: 'Financial',
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                data:  [1, 1, 1, 1, 1, 1]   
+                },
+                {
+                label: 'Programming',
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',                    
+                data: [1, 1, 1, 1, 1, 1]
+                }],
+                
+            labels:[1, 2, 3, 4, 5, 6]
+            
+        }
+    }
+
+    getDoughnutData = () => {
+        return {
+            labels: ["Social Media", "Streaming", "Gaming", "News", "Financial", "Programming"],
             datasets: [{
                 label: "My First dataset",
-                data: [0, 10, 5, 2, 20, 30, 45],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                data: [40, 10, 5, 2, 20, 30],
             }]
         }
     }
     render() {
+        defaults.global.legend.position = 'left'
         return (
 	        <div className="panel">
 		        <div className="panel-container">
@@ -42,7 +89,25 @@ class DataPanel extends Component {
 				        </div>
 				        <div className="panel-center-content">
 					        <div className="Data-Panel">
-						        <Doughnut data={this.getData()} options={{}} />
+                                <div className="graph">
+                                    <h3 className="h3-size">Website Categories</h3>
+                                    <Doughnut data={this.getDoughnutData()}/>
+                                    <HorizontalBar
+                                        data={this.getBarData()}
+                                        width={100}
+                                        height={50}
+                                        options={{
+                                            scales: {
+                                                xAxes: [{
+                                                    stacked: true
+                                                }],
+                                                yAxes: [{
+                                                    stacked: true
+                                                }]
+                                            }
+                                        }}
+                                    />
+                                </div>
 					        </div>
 				        </div>
 			        </div>
