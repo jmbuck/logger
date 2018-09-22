d2b3ed5d360f0a7541a97cbdf40d152eec01ffae
Killian-LeClainche <kleclain@purdue.edu>
2018-09-21 22:07:00 -0400

    Update specific data page

--- src/components/WebsitePanel.js
+++ src/components/WebsitePanel.js
@@ -4,6 +4,18 @@ import exitIcon from "../img/x.svg"
 import DataNav from './DataNav'
 
 class WebsitePanel extends Component {
+
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data : [
+                {name : "Facebook", visits : 5, time : "3 minutes", category : "Social Media"},
+                {name : "Youtube", visits : 2, time : "37 minutes", category : "Videos"}
+            ]}
+    }
+
     render() {
         return (
             <div className="panel">
@@ -18,19 +30,20 @@ class WebsitePanel extends Component {
 			                        <th>Number of Visits</th>
 			                        <th>Time Spent</th>
 			                        <th>Category</th>
+                                    <th>Delete</th>
 		                        </tr>
-		                        <tr>
-			                        <td>Facebook</td>
-			                        <td>5</td>
-			                        <td>3 minutes</td>
-			                        <td contentEditable='true'>Social Media</td>
-		                        </tr>
-		                        <tr>
-			                        <td>Youtube</td>
-			                        <td>2</td>
-			                        <td>37 minutes</td>
-			                        <td contentEditable='true'>Videos</td>
-		                        </tr>
+                                {
+                                    this.state.data.map((d) =>
+                                        <tr>
+                                            <td>{d.name}</td>
+                                            <td>{d.visits}</td>
+                                            <td>{d.time}</td>
+                                            <td>{d.category}</td>
+                                            <td>
+                                                <img src={exitIcon} style={{"filter" : "invert(100%)", "width": "25px", "height" : "25px", "cursor" : "pointer"}}/>
+                                            </td>
+                                        </tr>)
+                                }
 	                        </table>
                         </div>
                     </div>
--- src/components/YoutubePanel.js
+++ src/components/YoutubePanel.js
@@ -4,6 +4,19 @@ import exitIcon from "../img/x.svg"
 import DataNav from './DataNav'
 
 class YoutubePanel extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data : [
+                {name: "VSauce", time: "7 hours", visits : "102"},
+                {name: "Pewdiepie", time: "12 hours", visits: "234"},
+                {name: "Summoning Salt", time: "5 hours 37 minutes", visits: "25"}
+            ]
+        }
+    }
+
     render() {
         return (
 	        <div className="panel">
