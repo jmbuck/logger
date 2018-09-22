7d43f5cafcb1d702c4028b150dedd146588ee484
Ramsey Ali <ali59@purdue.edu>
2018-09-22 15:47:27 -0400

    incomplete Session Logic

--- public/Background.js
+++ public/Background.js
@@ -4,14 +4,18 @@
 
 import firebase from "firebase";
 
-var TabSessions = {};
+var SessionsArray = [];
 
 var startOfBrowsing = Date.now();
-var startOfSession = Date.now();
 var startYoutube;
 var endYoutube;
 var channel;
 var endOfSession;
+
+function domainRetreival(Stirng URL) {
+  var resultArray = URL.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img);
+  return resultArray[0];
+}
 
 
 function onActiveTabChange(activeInfo) {
@@ -20,10 +24,10 @@ function onActiveTabChange(activeInfo) {
       let url = tabs[0] != undefined ? tabs[0].url : null
       if(url != null) checkTab(url)
     });
-    endOfSession = Date.now(); //sessions are tab based.....for now.
-    var sessionTime = startOfSession - endOfSession;
+    //endOfSession = Date.now(); //sessions are tab based.....for now.
+    //var sessionTime = startOfSession - endOfSession;
     // We have the session time!
-    startOfSession = Date.now();
+    //startOfSession = Date.now();
     //alert("Tab " + activeInfo.tabId + " is now the active tab!");
 }
 
@@ -53,8 +57,68 @@ function onTabUpdate(tabId, changeInfo, tab) {
   //Logic for when a tab is updated
     //alert("Tab " + tabId + " has updated!");
     if (changeInfo.status == "loading" && changeInfo.url != undefined) {
+
       //alert("URL is now: " + changeInfo.url)
-      checkTab(changeInfo.url)
+      var sessionNotTracked = true;
+      var tabNotTracked = true;
+
+      for (var i = 0; i < SessionsArray.length; i++) {
+        //Are we already tracking this tab?
+        if (SessionsArray[i].tabid == tabId) {
+          tabNotTracked = false;
+          //Is the new URL in the same domain as what we were already tracking for the tab?
+          if (SessionsArray[i].domain == domainRetreival(changeInfo.url)) {
+            //Do nothing if it is - it's the same session
+          } else {
+            var finalSessionTime = Date.now() - SessionsArray[i].startOfSession;
+            //HERE: This marks the end of a prior session, what to do with SessionArray[i]'s data?'
+            SessionsArray.splice(i, 1); //Remove this old session.
+            //Are we already tracking this new session in another tab?
+            for (var i = 0; i < SessionsArray.length; i++) {
+              if (SessionsArray[i].domain == domainRetreival(changeInfo.url)) {
+                //We are tracking this session in another tab
+              } else {
+                //We are not tracking this session in another tab
+                var TabSession =  = {
+                  tabid: tabId,
+                  startOfSession: Date.now(),
+                  rawURL: changeInfo.url,
+                  domain: domainRetreival(changeInfo.url)
+                };
+                SessionsArray.push(TabSession);
+              }
+            }
+          }
+        }
+      }
+
+      sessionNotTracked = true;
+
+        //We are not tracking this tab
+        if (tabNotTracked) {
+          //Are we already tracking this session (on another tab)?
+            for (var i = 0; i < SessionsArray.length; i++) {
+              if (SessionsArray[i].domain == domainRetreival(changeInfo.url)) {
+                sessionNotTracked = false;
+              }
+            }
+            if (sessionNotTracked) {
+              //We're not tracking this session in any other tab
+              var TabSession =  = {
+                tabid: tabId,
+                startOfSession: Date.now(),
+                rawURL: changeInfo.url,
+                domain: domainRetreival(changeInfo.url)
+              };
+              SessionsArray.push(TabSession);
+            } else {
+              //We're tracking this session in another tab - do nothing
+            }
+          }
+
+
+      checkTab(changeInfo.url);
+
     } else if (changeInfo.status == "loading") {
       //alert("URL is the same.");
     } else {
@@ -130,7 +194,7 @@ function postVideoData(channel, timeWatched) {
     console.log(channel)
     console.log(timeWatched)
   }
-  
+
   channel = null;
 }
 
