28860a5c4c350826c3c92fda94af0f8a0cbe1d58
Jordan Buckmaster <buckmast@purdue.edu>
2018-10-12 20:27:30 -0400

    Add category editing and default category

--- src/components/WebsitePanel.js
+++ src/components/WebsitePanel.js
@@ -5,7 +5,10 @@ import DataNav from './DataNav'
 import { retrieveFirebaseWebsiteData,
     retrieveFirebaseWebsitesBlacklist,
     retrieveFirebaseWebsitesSettings,
-    deleteFirebaseWebsite} from "../logger-firebase";
+    deleteFirebaseWebsite,
+    retrieveDefaultCategory,
+    setWebsiteCategory,
+    } from "../logger-firebase";
 import { auth } from "../database/Auth";
 
 class WebsitePanel extends Component {
@@ -18,6 +21,8 @@ class WebsitePanel extends Component {
             data: [],
             settings: null,
             blacklist: null,
+            dropdownClass: 'hide',
+            categoryClass: '',
         }
     }
 
@@ -44,6 +49,18 @@ class WebsitePanel extends Component {
         }
         return false
     };
+
+    getDefaultCategory = (website, index) => {
+        let websites = [...this.state.data]
+        retrieveDefaultCategory(website, (data) => {
+            websites[index].category = data;
+            this.setState({ data: websites})
+        }); 
+    }
+
+    updateCategory = (website, category) => {
+        setWebsiteCategory(auth.currentUser.uid, website, category)
+    }
 
     handleDelete = (index) => {
         let data = [...this.state.data];
@@ -90,7 +107,38 @@ class WebsitePanel extends Component {
                                                 <td>{this.state.settings[d.name] ? (this.state.settings[d.name].visits ? d.visits : 'N/A') : d.visits}</td>
                                                 <td>{this.state.settings[d.name] ? (this.state.settings[d.name].time ? d.time : 'N/A') : d.time}</td>
                                                 <td>{this.state.settings[d.name] ? (this.state.settings[d.name].data ? d.data : 'N/A') : d.data}</td>
-                                                <td>{d.category}</td>
+                                                <td>
+                                                    <span className={`category ${this.state.categoryClass}`} onClick={(ev) => {
+                                                        ev.persist()
+                                                        if(!this.state.categoryClass) {
+                                                            this.setState({categoryClass: 'hide', dropdownClass: ''}, () => 
+                                                            ev.target.nextSibling.focus()
+                                                            )
+                                                        }
+                                                        } }>
+                                                        {d.category 
+                                                        ? `${d.category}`
+                                                        : this.getDefaultCategory(d, index)}
+                                                    </span>
+                                                    <select name="category" className={this.state.dropdownClass} onChange={(ev) => {
+                                                        this.updateCategory(d, ev.target.value)
+                                                        this.setState({categoryClass: '', dropdownClass: 'hide'})
+                                                        }} onBlur={() => {
+                                                        this.setState({categoryClass: '', dropdownClass: 'hide'})
+                                                        }} defaultValue={d.category}>
+                                                        <option value="">-</option>
+                                                        <option value="E-Commerce">E-Commerce</option>
+                                                        <option value="Personal">Personal</option>
+                                                        <option value="Political<">Political</option>
+                                                        <option value="News">News</option>
+                                                        <option value="Corporate">Corporate</option>
+                                                        <option value="Consultation">Consultation</option>
+                                                        <option value="Video">Video</option>
+                                                        <option value="Games">Games</option>
+                                                        <option value="Movies & TV">Movies & TV</option>
+                                                        <option value="Social Media">Social Media</option>
+                                                    </select>
+                                                </td>
                                                 <td>
                                                     <img src={exitIcon} onClick={() => this.handleDelete(index)} style={{"filter" : "invert(100%)", "width": "25px", "height" : "25px", "cursor" : "pointer"}}/>
                                                 </td>
